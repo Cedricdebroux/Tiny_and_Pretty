df986cc3f21b8ba28b8b90fa145c8eec77653402
unknown <patricia.corduant@gmail.com>
2021-08-02 15:16:02 +0200

    homepage

--- migrations/Version20210729102610.php
+++ migrations/Version20210729102610.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210727142430 extends AbstractMigration
+final class Version20210729102610 extends AbstractMigration
 {
     public function getDescription(): string
     {
