88772531269b8f19042de71cf9968dd36ba4f1e7
Debroux <71833601+Cedricdebroux@users.noreply.github.com>
2021-08-20 09:40:34 +0200

    baby conflict resolved

--- src/Form/SearchBaby.php
+++ src/Form/SearchBaby.php
@@ -12,7 +12,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class SearchType extends AbstractType
+class SearchBaby extends AbstractType
 {
 
     public function buildForm(FormBuilderInterface $builder, array $options)
