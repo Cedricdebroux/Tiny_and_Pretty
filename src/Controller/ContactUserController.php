3e5deec6f0464c3b82e6ba43929edd76c39b4534
Corduant Patricia <patricia.corduant@gmail.com>
2021-08-24 15:59:19 +0200

    contact

---
+++ src/Controller/ContactUserController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\ContactUser;
+use App\Form\ContactUserType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactUserController extends AbstractController
+{
+    #[Route('/contact/user', name: 'contact_user')]
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $contactuser = new ContactUser();
+
+        $form = $this->createForm(ContactUserType::class, $contactuser);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $entityManager->persist($contactuser);
+            $entityManager->flush();
+        }
+
+
+        return $this->render('contact_user/index.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
